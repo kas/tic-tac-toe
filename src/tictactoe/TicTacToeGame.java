db3a344c414790395c7e241c45ece1cb2136215c
Ken Schnall <kenneth.schnall@gmail.com>
2016-02-22 22:02:06 -0500

    updated project name

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tictactoe;
+package TicTacToe;
 
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.Scanner;
